dc7a294c5f4559b58fc619b8e98421ec37b32b40
zanetaam <83244563+zanetaam@users.noreply.github.com>
2022-04-26 02:08:09 -0700

    Add files via upload

--- Project_EE120.ino
+++ Project_EE120.ino
@@ -1,7 +1,16 @@
+#include <hcsr04.h>
 
+#include <LiquidCrystal.h>
 
+#define TRIG_PIN 10
+#define ECHO_PIN 7
+HCSR04 hcsr04(TRIG_PIN, ECHO_PIN, 20, 4000);
 // Library for LCD Screen
 #include <LiquidCrystal.h>
+
+const int motorPin1  = 9;
+const int motorPin2  = 13;
+
 
 // LCD pins connected to Arduino Board accordingly i.e RS on LCD screen goes to pin 12 on arduino board) 
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2; 
@@ -18,6 +27,24 @@ int backLight = 8; // pin 13 will control the backlight
 
 void setup() 
 {
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  { Serial.begin(9600); }
+  if ( hcsr04.distanceInMillimeters() < 15)
+  {
+    lcd.noDisplay();
+    digitalWrite(LED, LOW);
+    delay(20000);
+    digitalWrite(motorPin1, LOW);
+     
+    
+     
+  }
+  else
+  {
+    digitalWrite(LED, HIGH); //Turn red LED on  
+  delay (1);
+   digitalWrite(motorPin1, HIGH);
   // Turning on the screnlight
   pinMode(backLight, OUTPUT); 
 
@@ -29,13 +56,15 @@ void setup()
   // start of line 2 
   lcd.setCursor(5, 7);
   
-  lcd.print("-Zaneta :)");
-}
+  lcd.print("-Z&N :)");
 
+  
+}
+}
 void loop() 
 
 {
- // screen is till on during loop
+ // screen is still on during loop
  digitalWrite(backLight, OUTPUT);
  
   // setting the counter up 
@@ -46,7 +75,7 @@ void loop()
   
  
 // if the timer is less than 10 seconds the LED remains on
-if (((millis() / 1000) < 10))
+  if (((millis() / 1000) < 10))
 {
   digitalWrite(LED, HIGH); //Turn red LED on  
   delay (1);
@@ -57,11 +86,13 @@ if (((millis() / 1000) < 10))
  { 
   digitalWrite(LED, LOW); //Turn red LED off    
   delay (1);//Leave off for redOffTime
-}
+  }
 
 // once the timer reaches 10 seconds the display has no text to go along with the LED timing 
-if (((millis() / 1000) == 10))
+  if (((millis() / 1000) == 10))
   {
     lcd.noDisplay();
+    digitalWrite(motorPin1, LOW);
+    delay(2000);
   }
  }
